a609f0663e6cbcebff5f5d91173df8794fe61b12
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-01 16:14:08 +0300

    Nested Routing Code-Along

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,28 @@
 import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
 
-function Home() {
-  const [users, setUsers] = useState([]);
+function Home(){
+    const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
-  });
+    useEffect(() =>{
+        fetch("http://localhost:4000/users")
+        .then(r => r.json())
+        .then(data => setUsers(data))
+        .catch(error => console.error(error));
+    }, []);
 
-  return (
-    <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <h1>Home!</h1>
-        {userList}
-      </main>
-    </>
-  );
+    const userList = users.map(user =>{
+        return <UserCard key={user.id} user={user}/>;
+    });
+
+    return (
+        <main>
+            <h1>Home!</h1>
+            <Outlet />
+            {userList}
+        </main>
+    );
 };
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -4,26 +4,32 @@ import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
 
+
 const routes = [
   {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+           {
+              path: "/",
+              element: <Home />,
+              children: [
+                  {
+                      path: "/profile/:id",
+                      element: <UserProfile />
+                  }
+              ]
+          }, 
+          {
+              path: "/about",
+              element: <About />
+          },
+          {
+              path: "/login",
+              element: <Login />
+          }
+      ]
   }
 ];
 
